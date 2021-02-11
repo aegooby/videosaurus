a65325b1042cf550ef7de901a4ad6e9c6da0c13c
aegooby <aegooby@gmail.com>
2021-02-11 06:58:49 +0300

    Small fixes

--- app/index.ts
+++ app/index.ts
@@ -7,8 +7,6 @@ import * as jszip from "jszip";
 /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
 /* @ts-ignore */
 import * as native from "../engine/build/Release/videosaurus.node";
-import { fstat } from "fs";
-
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
@@ -23,17 +21,16 @@ icon_image.isMacTemplateImage = true;
 electron.app.dock.setIcon(icon_image);
 
 const extensions_dir = path.join(__dirname, "../../extensions");
-const devtools_zip = path.join(extensions_dir, "react-devtools.zip");
-const devtools_dir = path.join(extensions_dir, "react-devtools");
+const react_devtools_zip = path.join(extensions_dir, "react-devtools.zip");
+const react_devtools_dir = path.join(extensions_dir, "react-devtools");
 
 async function unzip_devtools(): Promise<void>
 {
-    const buffer = await fs.promises.readFile(path.resolve(devtools_zip));
+    const buffer = await fs.promises.readFile(path.resolve(react_devtools_zip));
     const zip = await jszip.loadAsync(buffer);
     const keys = Object.keys(zip.files);
     async function unzip(filename: string): Promise<void>
     {
-        console.log(filename);
         const is_file = !zip.files[filename].dir;
         const full_path = path.join(extensions_dir, filename);
         const directory = is_file && path.dirname(full_path) || full_path;
@@ -43,24 +40,19 @@ async function unzip_devtools(): Promise<void>
         if (is_file)
             await fs.promises.writeFile(full_path, content);
     }
-    keys.map(unzip);
+    await Promise.all(keys.map(unzip));
 }
 
 async function ready(): Promise<void>
 {
-    try
-    {
-        // console.log(native.add(5, 10));
-        await fs.promises.access(devtools_dir);
-    }
-    catch (error) 
-    {
-        await unzip_devtools();
-    }
+    /** @todo Temp. */
+    console.log(native.add(5, 10));
+    try { await fs.promises.access(react_devtools_dir); }
+    catch (error) { await unzip_devtools(); }
     try
     {
         create_window();
-        await electron.session.defaultSession.loadExtension(devtools_dir);
+        await electron.session.defaultSession.loadExtension(react_devtools_dir);
     }
     catch (error) { console.log(error); }
 }
