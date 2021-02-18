d60607bbfbbad4fe37abcc8459627389e322a5f5
aegooby <aegooby@gmail.com>
2021-02-18 10:24:49 +0300

    Updated dependencies

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,5 +1,2 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/** @note Causes bugs on exit, disable for now. */
-// const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-
 module.exports = [];
